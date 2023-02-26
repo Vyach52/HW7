4bff07dc476bab116f5da55403ccf95347424070
Vyach52 <Sslavih81@gmail.com>
2023-02-26 19:14:23 +0300

    Циклы часть 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        int salary = 15_000;
+        int total = 0;
+        int i = 1;
+        while (total <= 2_459_000  ) {
+            i++;
 
+            total = total + salary;
+            System.out.println("Месяц "+ i + " Сумма накоплений " + total);}
+        { System.out.println(
+
+        );}
     }
 }
