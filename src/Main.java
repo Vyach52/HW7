04a8eb0853c317ffd921467c45565c7cf9d51ba7
Vyach52 <Sslavih81@gmail.com>
2023-03-13 19:07:25 +0300

    Циклы часть 2

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        { int salary = 15_000;
-        int total = 0;
-        int i = 1;
-        while (total <= 2_459_000) {
-            i++;
-            total = total + salary;
-            System.out.println("Месяц " + i + " Сумма накоплений " + total);
-        }
+        {
+            int salary = 15_000;
+            int total = 0;
+            int i = 1;
+            while (total <= 2_459_000) {
+                i++;
+                total = total + salary;
+                System.out.println("Месяц " + i + " Сумма накоплений " + total);
+            }
 
             System.out.println(
 
             );
         }
-        for (int i = 1; i <= 10; i = i +1)
-            System.out.print(" "+i+" " );
+        for (int i = 1; i <= 10; i = i + 1)
+            System.out.print(" " + i + " ");
         System.out.println(
 
         );
         for (int a = 10; a >= 1; a = a - 1)
-            System.out.print(" "+a+" " );
+            System.out.print(" " + a + " ");
         System.out.println(
 
         );
-            { int population = 12_000_000;
+        {
+            int population = 12_000_000;
             int birthRate = (12_000_000 / 1000) * 17;
             int mortality = (12_000_000 / 1000) * 8;
             int populationNext = birthRate - mortality;
             int year = 2023;
-                while (year <= 2033) {
-                    population = population + populationNext;
-                    year = year + 1;
-                    System.out.println("Год " + year + ", численность населения составляет " + population + " миллионов человек.");}
+            while (year <= 2033) {
+                population = population + populationNext;
+                year = year + 1;
+                System.out.println("Год " + year + ", численность населения составляет " + population + " миллионов человек.");
+            }
         }
     }
 }
